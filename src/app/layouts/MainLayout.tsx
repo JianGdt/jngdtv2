7c544a6cbefbfed11c2251cb153b6bbd92c08b05
JianGodito <jgodito27@gmail.com>
2024-01-17 18:22:05 -0500

    add loading

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,27 +1,27 @@
-// 'use client'
-// import React, { useState, useEffect } from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 import NavItem from "@/app/layouts/NavItem";
 import HeaderLayout from "@/app/layouts/HeaderLayout";
 import SocialMedia from "@/app/layouts/components/SocialMedia";
 import MyName from "@/app/layouts/components/MyName";
 import AppContainer from "@/app/layouts/components/AppContainer";
 import Footer from "@/app/layouts/components/Footer";
-// import Loading from '@/app/components/Loading';
+import Loading from '@/app/components/Loading';
 
 export default function MainLayout(props: { children: React.ReactNode }) {
-  // const [isLoading, setLoading] = useState<boolean>(true);
+  const [isLoading, setLoading] = useState<boolean>(true);
 
-  // useEffect(() => {
-  //     setTimeout(() => {
-  //       setLoading(false);
-  //     }, 5000);
-  // }, []); 
+  useEffect(() => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 5000);
+  }, []); 
 
   return (
     <>
-      {/* {isLoading ? (
+      {isLoading ? (
         <Loading />
-      ) : ( */}
+      ) : (
         <>
           <AppContainer>
             <HeaderLayout>
@@ -33,7 +33,7 @@ export default function MainLayout(props: { children: React.ReactNode }) {
           {props.children}
           <Footer />
         </>
-      {/* )} */}
+      )}
     </>
   );
 }
